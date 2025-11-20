937170f97f8174bbb0a711b67a273f9f6682a07e
Elizaveta <el1zaveta-vladimirova@bk.ru>
2025-11-20 22:01:35 +0300

    Разработать XAML-страницы, обработчики событий и модальные окна для сущности Booking

---
+++ CrystalCove/Views/Booking/BookingPageView.xaml.cs
@@ -0,0 +1,101 @@
+﻿using CrystalCove.Models;
+using CrystalCove.Services;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace CrystalCove.Views
+{
+    public partial class BookingPageView : Page
+    {
+        public BookingPageView()
+        {
+            InitializeComponent();
+            RefreshBookingData();
+        }
+
+        // Метод для обновления данных о бронировании
+        private void RefreshBookingData()
+        {
+            BookingDataGrid.ItemsSource = BookingService.GetAllBookingWithStaffClient();
+        }
+
+        // Метод для поиска родительского элемента определенного типа
+        private static T FindPerent<T>(DependencyObject child) where T : DependencyObject
+        {
+            DependencyObject perentObject = VisualTreeHelper.GetParent(child);
+            if (perentObject == null)
+            {
+                return null;
+            }
+            T perent = perentObject as T;
+            return perent ?? FindPerent<T>(perentObject);
+        }
+
+        // Обработчик события для кнопки добавления бронирования
+        private void AddBooking_Click(object sender, RoutedEventArgs e)
+        {
+            var hotelHomeView = FindPerent<HotelHomeView>(this);
+            hotelHomeView?.ShowOverlay();
+
+            //if (UserService.CurrentStaff != null)
+            //{
+            //    using (AppDbContext hotelDbContext = new AppDbContext())
+            //    {
+            //        var currentClient = hotelDbContext.Clients.FirstOrDefault();
+
+            //        if (currentClient != null)
+            //        {
+            //            AddBookingView addBookingView = new AddBookingView(BookingDataGrid, UserService.CurrentStaff, currentClient);
+            //            addBookingView.ShowDialog();
+
+            //            hotelHomeView?.HideOverlay();
+
+            //            RefreshBookingData();
+            //        }
+            //        else
+            //        {
+            //            MessageBox.Show("Не удалось определить текущего клиента.", "Ошибка",
+            //            MessageBoxButton.OK, MessageBoxImage.Error);
+            //            hotelHomeView?.HideOverlay();
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Не удалось определить текущего сотрудника.", "Ошибка",
+            //    MessageBoxButton.OK, MessageBoxImage.Error);
+            //    hotelHomeView?.HideOverlay();
+            //}
+        }
+
+        // Обработчик события для кнопки изменения бронирования
+        private void EditBooking_Click(object sender, RoutedEventArgs e)
+        {
+            var hotelHomeView = FindPerent<HotelHomeView>(this);
+            hotelHomeView?.ShowOverlay();
+
+            using (AppDbContext hotelDbContext = new AppDbContext())
+            {
+                var currentClient = hotelDbContext.Clients.FirstOrDefault();
+                UpdateBookingView updateBookingView = new UpdateBookingView(BookingDataGrid, new Staff(), currentClient);
+                updateBookingView.ShowDialog();
+
+                hotelHomeView?.HideOverlay();
+
+                RefreshBookingData();
+            }
+        }
+
+        // Обработчик события для кнопки удаления бронирования
+        private void DeleteBooking_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedBooking = BookingDataGrid.SelectedItem as Booking;
+            if (selectedBooking != null)
+            {
+                BookingService.DeleteBooking(selectedBooking);
+                RefreshBookingData();
+            }
+        }
+    }
+}
